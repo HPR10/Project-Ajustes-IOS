c4ad00c7392186e8ef802267b0ee933b9084c3cc
Hugo Pinheiro <hugopinheiro@MacBook-Pro-de-Hugo.local>
2021-09-09 23:00:53 -0300

    third commit

--- Ajustes/ContentView.swift
+++ Ajustes/ContentView.swift
@@ -8,26 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
-     
+    
+    @State var previewIndex = 0
+    
     var body: some View {
-        
-        
         NavigationView {
-            HStack {
+            
+            
                 Form {  // Form é tipo uma tableview.
                     
-                    Section(header: Text("Display"),
-                            footer: Text("Configurações de rede")) {
+                    Section {
+                        HStack{
+                            Image(systemName: "person.circle.fill")
+                                .font(.system(size: 75))
+                                .foregroundColor(.gray)
+                            VStack(alignment:.leading){
+                                Button("Apple Lab"){
+                                }.font(.callout)
+                                Text("Apple ID, icloud, media and more")
+                                    .font(.caption)
+                                    .foregroundColor(.secondary)
+                            }
+                            
+                        }
+                    }
+                    
+                    Section {
                         
                         HStack {
                             Image(systemName: "airplane")
+                                .frame(width: 25, height: 25)
+                                .foregroundColor(.white)
+                                .background(Color.yellow)
+                                .clipShape(Circle())
                             Text("Modo Avião")
                                // .padding()  // espaço que aceita parâmetro
-                            Toggle(isOn: .constant(true)) {
+                            Toggle(isOn: .constant(false)) {
                             }
                         }
+                        
                         HStack {    // elementos na horizontal
                             Image(systemName: "wifi")
+                                .frame(width: 25, height: 25)
+                                .foregroundColor(.white)
+                                .background(Color.blue)
+                                .clipShape(Circle())
                             Text("Wi-Fi")
                             Spacer()    // espaço extremo
                             Text("Uai-Fai")
@@ -35,52 +60,61 @@ struct ContentView: View {
                                 .foregroundColor(Color.gray)
                         }
                         
+                        HStack {
+                            Image(systemName: "airpods")
+                                .frame(width: 25, height: 25)
+                                .foregroundColor(.white)
+                                .background(Color.blue)
+                                .clipShape(Circle())
+                            Text("Bluetooth")
+                            Spacer()
+                            Text("On")
+                                .font(.callout)
+                                .foregroundColor(Color.gray)
+                    }
+                
+                        HStack{
+                            Image(systemName: "antenna.radiowaves.left.and.right")
+                                .frame(width: 25, height: 25)
+                                .foregroundColor(.white)
+                                .background(Color.green)
+                                .clipShape(Circle())
+                            Picker(selection:$previewIndex, label:Text("Cellular")){
+                            }
+                        }
+                        
+                        HStack{
+                            Image(systemName: "personalhotspot")
+                                .frame(width: 25, height: 25)
+                                .foregroundColor(.white)
+                                .background(Color.green)
+                                .clipShape(Circle())
+                            Picker(selection:$previewIndex, label:Text("Personal Hotspot")){
+                            }
+                        }
+                        
+                        HStack {
+                            Image(systemName: "network")
+                                .frame(width: 25, height: 25)
+                                .foregroundColor(.white)
+                                .background(Color.blue)
+                                .clipShape(Circle())
+                            Text("VPN")
+                            Toggle(isOn: .constant(false)) {
+                            }
+                        }
+                        
                     }
                     
-                    HStack{
-                        Image(systemName: "folder.fill.badge.plus")
-                        Text("Notifications")
-                    }
-                    HStack{
-                        Image(systemName: "speaker.3.fill")
-                        Text("Sounds")
-                    }
-                    Section(footer: Text("Redes sociais")) {
-                        Label("Me siga no twitter @hp", systemImage: "link")
-                            .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-                            .font(.system(size: 16, weight: .heavy))
-                    }
-            
-                }
-                .navigationTitle("Ajustes")
-               
-            }
-            
-        }
+                }.navigationBarTitle("Ajustes")
       
-    }
-}
-
-
-
-
-
-// https://www.youtube.com/watch?v=fktqZRRQgyU
-
-
-
-
-
-
-
-
-
-
-
-
-
+            }
+        }
+    
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
+        }
     }
 }
